4765427fade1e5b1d06f5634ff6516e2c03466a3
Corey <corey.amano@gmail.com>
2021-09-05 22:22:59 -0700

    add solutions for problems 6-8

--- convert_data_js.js
+++ convert_data_js.js
@@ -67,3 +67,49 @@ Object.entries(people).forEach(function ([id, person]) {
   peopleArray.push(person);
 });
 console.log(peopleArray);
+
+//  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
+//     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
+
+var words = ["do", "or", "do", "not"];
+var wordFrequencies = {};
+var index = 0;
+while (index < words.length) {
+  var word = words[index];
+  if (wordFrequencies[word] === undefined) {
+    wordFrequencies[word] = 0;
+  }
+  wordFrequencies[word] += 1;
+  index++;
+}
+console.log(wordFrequencies);
+
+//  7. Convert a hash into a flat array containing all the hash’s keys and values.
+//     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+var hash = { a: 1, b: 2, c: 3, d: 4 };
+var flattenedArray = [];
+Object.entries(hash).forEach(function ([key, value]) {
+  flattenedArray.push(key);
+  flattenedArray.push(value);
+});
+console.log(flattenedArray);
+
+//  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
+//     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+var priceObject = { chair: 75, book: 15 };
+var items = [
+  { name: "chair", color: "red", weight: 10 },
+  { name: "book", color: "black", weight: 1 },
+];
+var combinedObject = {};
+var index = 0;
+while (index < items.length) {
+  var item = items[index];
+  var name = item.name;
+  var color = item.color;
+  var weight = item.weight;
+  var price = priceObject[name];
+  combinedObject[name] = { price: price, color: color, weight: weight };
+  index += 1;
+}
+console.log(combinedObject);
